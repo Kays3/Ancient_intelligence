f01282f51636957317f5bdb60fd80b4341a17d44
Kays3 <k.dauyey.bio.nu@gmail.com>
2021-06-27 22:22:33 +0900

    editted figure1

--- figure 1.R
+++ figure 1.R
@@ -1,9 +1,10 @@
-#june 11, 2021
+#june 27, 2021
 #1402 snps common 1000G and Jomon _Afanasievo
 #genotypes extracted
 #polygenic score calculated
 #EA proxy replication for all snps...
 #uncomment to save plots etc
+#paper editing plot
 #Author kaisar dauyey
 
 rm(list = ls())
@@ -30,11 +31,24 @@ GP3<- GP_nogomi_j %>% select(subject,effect_sum) %>%
   group_by(subject)
 GP3<-GP3[-c(1657,1658,1659,1660,1661),]
 
+GP32<-GP_nogomi_j[,-1]
 
 jomon_afa<-GP_nogomi_j[c(1657,1658,1659,1660,1661),]
 
 
+Stats <- summarize_all(GP32, mean)
+Stats["subject"]<-"M"
+Stats <- Stats[c(7,1,2,3,4,5,6)]
 
+
+
+cuts<-jomon_afa
+
+cuts<-rbind(cuts,Stats)
+tuta<-c("AM","AS1","AF","AS2","F23","M")
+cuts$subject<-tuta
+
+head(cuts)
 
 head(jomon_afa)
 
@@ -81,7 +95,168 @@ afa_s2<-jomon_afa$effect_sum[4]
 z_afa_s2 <- (afa_s2 - pop_mean242) / pop_sd242
 p_yellow_afa_s2 <- pnorm(z_afa_s2,lower.tail = TRUE, log.p = FALSE) 
 
+#check for colored plots at the end of the script
 
+
+#no legend figure 1 1402
+# ggplot  NORMAL distribution for 1000 GP vs F23 non-standrd
+b<- ggplot(GP3, aes(x = effect_sum),show.legend = FALSE) +
+  stat_function(
+    fun = dnorm,
+    args = with(GP3, c(mean = pop_mean242, sd = pop_sd242))
+  )+  
+  geom_vline(mapping = aes(xintercept = effect_sum),
+             data = cuts,
+             show.legend = FALSE) +
+  #Set the size of the plotting window
+  theme_pubclean()+
+  
+  theme(
+    plot.title = element_text(angle = 0, size = 7, vjust = 1),
+    plot.subtitle = element_text(angle = 0, size = 7, vjust = 1),
+    plot.caption = element_text(angle = 0, size = 7, vjust = 1),
+    
+    axis.text.x = element_text(angle = 45, size = 7, hjust = 1.10),
+    axis.text.y = element_text(angle = 0, size = 7, vjust = 0.5),
+    axis.title = element_text(size = 7),
+    axis.title.x = element_text(size = 7),
+    axis.title.y = element_text(size = 7),
+    
+    
+  )+
+  #Set x- and y-axes labels
+  xlab('Score') +
+  ylab('Density')
+
+
+
+b
+# ggsave(filename = "~/Desktop/Figure1A_nolegend.pdf", plot = b, width = 17.5, height = 10, dpi = 300, units = "cm")
+
+
+#9k work
+
+#data loading
+GP_9k<-read_csv("PGS_1000G_J_Afa_9k.csv")
+
+GP3_9k<- GP_9k %>% select(subject,effect_sum) %>%
+  group_by(subject)
+GP3_9k<-GP3_9k[-c(1657,1658,1659,1660,1661),]
+
+
+jomon_afa_9k<-GP_9k[c(1657,1658,1659,1660,1661),]
+
+GP32_9k<-GP_9k[,-1]
+
+
+Stats_9k <- summarize_all(GP32_9k, mean)
+Stats_9k["subject"]<-"M"
+Stats_9k <- Stats_9k[c(7,1,2,3,4,5,6)]
+
+
+
+cuts_9k<-jomon_afa_9k
+
+cuts_9k<-rbind(cuts_9k,Stats_9k)
+tuta<-c("AM","AS1","AF","AS2","F23","M")
+cuts_9k$subject<-tuta
+
+head(cuts_9k)
+
+
+
+
+head(jomon_afa_9k)
+
+
+
+#####
+#9k SNPs pipeline 
+#Afanasievo
+
+#POPULATION PARAMETER CALCULATIONS
+pop_sd_9k <- sd(GP3_9k$effect_sum)*sqrt((length(GP3_9k$effect_sum)-1)/(length(GP3_9k$effect_sum)))
+pop_mean_9k <- mean(GP3_9k$effect_sum)
+# sd function in R uses the sample standard deviation
+# and not the population standard deviation, difference for 
+
+
+#z score estimation for F23
+j9k<-jomon_afa_9k$effect_sum[5]
+z9k <- (j9k - pop_mean_9k) / pop_sd_9k
+p_yellow_j9k <- pnorm(z9k,lower.tail = TRUE, log.p = FALSE)  
+
+
+
+#z score estimation for Afa_mother
+afa_m_9k<-jomon_afa_9k$effect_sum[1]
+z_afa_m_9k <- (afa_m_9k - pop_mean_9k) / pop_sd_9k
+p_yellow_afa_m_9k <- pnorm(z_afa_m_9k,lower.tail = TRUE, log.p = FALSE) 
+
+
+
+#z score estimation for Afa_son1
+afa_s1_9k<-jomon_afa_9k$effect_sum[2]
+z_afa_s1_9k <- (afa_s1_9k - pop_mean_9k) / pop_sd_9k
+p_yellow_afa_s1_9k <- pnorm(z_afa_s1_9k,lower.tail = TRUE, log.p = FALSE) 
+
+
+#z score estimation for Afa_father
+afa_f_9k<-jomon_afa_9k$effect_sum[3]
+z_afa_f_9k <- (afa_f_9k - pop_mean_9k) / pop_sd_9k
+p_yellow_afa_f_9k <- pnorm(z_afa_f_9k,lower.tail = TRUE, log.p = FALSE) 
+
+#z score estimation for Afa_son2
+afa_s2_9k<-jomon_afa_9k$effect_sum[4]
+z_afa_s2_9k <- (afa_s2_9k - pop_mean_9k) / pop_sd_9k
+p_yellow_afa_s2_9k <- pnorm(z_afa_s2_9k,lower.tail = TRUE, log.p = FALSE) 
+
+#plot no legend
+
+b2<- ggplot(GP3_9k, aes(x = effect_sum),show.legend = FALSE) +
+  stat_function(
+    fun = dnorm,
+    args = with(GP3_9k, c(mean = pop_mean_9k, sd = pop_sd_9k))
+  )+  
+  geom_vline(mapping = aes(xintercept = effect_sum),
+             data = cuts_9k,
+             show.legend = FALSE) +
+  #Set the size of the plotting window
+  theme_pubclean()+
+  
+  theme(
+    plot.title = element_text(angle = 0, size =  7,   vjust = 1),
+    plot.subtitle = element_text(angle = 0, size =  7,   vjust = 1),
+    plot.caption = element_text(angle = 0, size =  7,   vjust = 1),
+    
+    axis.text.x = element_text(angle = 45, size =  7,   hjust = 1.10),
+    axis.text.y = element_text(angle = 0, size =  7,   vjust = 0.5),
+    axis.title = element_text(size =  7  ),
+    axis.title.x = element_text(size =  7 ),
+    axis.title.y = element_text(size =  7  ),
+    
+  )+
+  #Set x- and y-axes labels
+  xlab('Score') +
+  ylab('Density')
+
+b2
+
+
+
+Figure1 <- plot_grid(b, b2, labels = "AUTO", ncol = 1, align = 'v')
+
+Figure1
+
+#ggsave("~/Desktop/Figure1_nolegend.pdf", Figure1, width=17.5, height=15, units="cm", dpi=300)
+
+
+
+
+#with LEGENDS COLORS
+#
+#
+#
 #change labels per number appropriately
 # ggplot  NORMAL distribution for 1000 GP vs F23 non-standrd
 b<- ggplot(GP3, aes(x = GP3$effect_sum),) +
@@ -143,65 +318,6 @@ b<- ggplot(GP3, aes(x = GP3$effect_sum),) +
 b
 # ggsave(filename = "~/Desktop/Figure1A.pdf", plot = b, width = 17.5, height = 10, dpi = 300, units = "cm")
 
-#9k work
-
-#data loading
-GP_9k<-read_csv("PGS_1000G_J_Afa_9k.csv")
-
-GP3_9k<- GP_9k %>% select(subject,effect_sum) %>%
-  group_by(subject)
-GP3_9k<-GP3_9k[-c(1657,1658,1659,1660,1661),]
-
-
-jomon_afa_9k<-GP_9k[c(1657,1658,1659,1660,1661),]
-
-
-
-
-head(jomon_afa_9k)
-
-
-
-#####
-#9k SNPs pipeline 
-#Afanasievo
-
-#POPULATION PARAMETER CALCULATIONS
-pop_sd_9k <- sd(GP3_9k$effect_sum)*sqrt((length(GP3_9k$effect_sum)-1)/(length(GP3_9k$effect_sum)))
-pop_mean_9k <- mean(GP3_9k$effect_sum)
-# sd function in R uses the sample standard deviation
-# and not the population standard deviation, difference for 
-
-
-#z score estimation for F23
-j9k<-jomon_afa_9k$effect_sum[5]
-z9k <- (j9k - pop_mean_9k) / pop_sd_9k
-p_yellow_j9k <- pnorm(z9k,lower.tail = TRUE, log.p = FALSE)  
-
-
-
-#z score estimation for Afa_mother
-afa_m_9k<-jomon_afa_9k$effect_sum[1]
-z_afa_m_9k <- (afa_m_9k - pop_mean_9k) / pop_sd_9k
-p_yellow_afa_m_9k <- pnorm(z_afa_m_9k,lower.tail = TRUE, log.p = FALSE) 
-
-
-
-#z score estimation for Afa_son1
-afa_s1_9k<-jomon_afa_9k$effect_sum[2]
-z_afa_s1_9k <- (afa_s1_9k - pop_mean_9k) / pop_sd_9k
-p_yellow_afa_s1_9k <- pnorm(z_afa_s1_9k,lower.tail = TRUE, log.p = FALSE) 
-
-
-#z score estimation for Afa_father
-afa_f_9k<-jomon_afa_9k$effect_sum[3]
-z_afa_f_9k <- (afa_f_9k - pop_mean_9k) / pop_sd_9k
-p_yellow_afa_f_9k <- pnorm(z_afa_f_9k,lower.tail = TRUE, log.p = FALSE) 
-
-#z score estimation for Afa_son2
-afa_s2_9k<-jomon_afa_9k$effect_sum[4]
-z_afa_s2_9k <- (afa_s2_9k - pop_mean_9k) / pop_sd_9k
-p_yellow_afa_s2_9k <- pnorm(z_afa_s2_9k,lower.tail = TRUE, log.p = FALSE) 
 
 
 
@@ -264,10 +380,3 @@ b2<- ggplot(GP3_9k, aes(x = GP3_9k$effect_sum),) +
                                                                      "Mean 1000 Genomes Project" = "#7E6148FF"))
 
 b2
-
-Figure1 <- plot_grid(b, b2, labels = "AUTO", ncol = 1, align = 'v')
-
-Figure1
-
-ggsave("~/Desktop/Figure1.pdf", Figure1, width=17.5, height=15, units="cm", dpi=300)
-
