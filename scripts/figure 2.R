1d82d1b117aa5a42107a85984faf11ca27516ab4
Kays3 <k.dauyey.bio.nu@gmail.com>
2022-03-24 01:36:03 +0900

    figure 2 corrected

--- scripts/figure 2.R
+++ scripts/figure 2.R
@@ -1,4 +1,4 @@
-#june 15, 2021
+#March 24, 2022
 #9k snps common 1000G and Jomon _Afanasievo
 #genotypes extracted
 #polygenic score calculated
@@ -87,37 +87,37 @@ which(pca_1402$popa == "Afanasievo_f")
 spp_1402 <- rep(NA, length(pca_1402$popa))
 
 #pca[,1]<- phase3[,3]
-spp_1402[grep("CHB", pca_1402$popa)] <- "East_Asia"
-spp_1402[grep("JPT", pca_1402$popa)] <- "East_Asia"
-spp_1402[grep("CHS", pca_1402$popa)] <- "East_Asia"
-spp_1402[grep("CDX", pca_1402$popa)] <- "East_Asia"
-spp_1402[grep("KHV", pca_1402$popa)] <- "East_Asia"
-spp_1402[grep("CEU", pca_1402$popa)] <- "Europe"
-spp_1402[grep("TSI", pca_1402$popa)] <- "Europe"
-spp_1402[grep("FIN", pca_1402$popa)] <- "Europe"
-spp_1402[grep("GBR", pca_1402$popa)] <- "Europe"
-spp_1402[grep("IBS", pca_1402$popa)] <- "Europe"
-spp_1402[grep("YRI", pca_1402$popa)] <- "Africa"
-spp_1402[grep("LWK", pca_1402$popa)] <- "Africa"
-spp_1402[grep("GWD", pca_1402$popa)] <- "Africa"
-spp_1402[grep("MSL", pca_1402$popa)] <- "Africa"
-spp_1402[grep("ESN", pca_1402$popa)] <- "Africa"
-spp_1402[grep("ASW", pca_1402$popa)] <- "Africa"
-spp_1402[grep("ACB", pca_1402$popa)] <- "Africa"
-spp_1402[grep("MXL", pca_1402$popa)] <- "Ad_Mixed_American"
-spp_1402[grep("PUR", pca_1402$popa)] <- "Ad_Mixed_American"
-spp_1402[grep("CLM", pca_1402$popa)] <- "Ad_Mixed_American"
-spp_1402[grep("PEL", pca_1402$popa)] <- "Ad_Mixed_American"
-spp_1402[grep("GIH", pca_1402$popa)] <- "South_Asia"
-spp_1402[grep("PJL", pca_1402$popa)] <- "South_Asia"
-spp_1402[grep("BEB", pca_1402$popa)] <- "South_Asia"
-spp_1402[grep("STU", pca_1402$popa)] <- "South_Asia"
-spp_1402[grep("ITU", pca_1402$popa)] <- "South_Asia"
-spp_1402[grep("Jomon", pca_1402$popa)] <- "Jomon"
-spp_1402[grep("Afanasievo_s2", pca_1402$popa)] <- "Afanasievo"
-spp_1402[grep("Afanasievo_s1", pca_1402$popa)] <- "Afanasievo"
-spp_1402[grep("Afanasievo_m", pca_1402$popa)] <- "Afanasievo"
-spp_1402[grep("Afanasievo_f", pca_1402$popa)] <- "Afanasievo"
+spp_1402[grep("CHB", pca_1402$popa)] <- "EAS"
+spp_1402[grep("JPT", pca_1402$popa)] <- "EAS"
+spp_1402[grep("CHS", pca_1402$popa)] <- "EAS"
+spp_1402[grep("CDX", pca_1402$popa)] <- "EAS"
+spp_1402[grep("KHV", pca_1402$popa)] <- "EAS"
+spp_1402[grep("CEU", pca_1402$popa)] <- "EUR"
+spp_1402[grep("TSI", pca_1402$popa)] <- "EUR"
+spp_1402[grep("FIN", pca_1402$popa)] <- "EUR"
+spp_1402[grep("GBR", pca_1402$popa)] <- "EUR"
+spp_1402[grep("IBS", pca_1402$popa)] <- "EUR"
+spp_1402[grep("YRI", pca_1402$popa)] <- "AFR"
+spp_1402[grep("LWK", pca_1402$popa)] <- "AFR"
+spp_1402[grep("GWD", pca_1402$popa)] <- "AFR"
+spp_1402[grep("MSL", pca_1402$popa)] <- "AFR"
+spp_1402[grep("ESN", pca_1402$popa)] <- "AFR"
+spp_1402[grep("ASW", pca_1402$popa)] <- "AFR"
+spp_1402[grep("ACB", pca_1402$popa)] <- "AFR"
+spp_1402[grep("MXL", pca_1402$popa)] <- "AMR"
+spp_1402[grep("PUR", pca_1402$popa)] <- "AMR"
+spp_1402[grep("CLM", pca_1402$popa)] <- "AMR"
+spp_1402[grep("PEL", pca_1402$popa)] <- "AMR"
+spp_1402[grep("GIH", pca_1402$popa)] <- "SAS"
+spp_1402[grep("PJL", pca_1402$popa)] <- "SAS"
+spp_1402[grep("BEB", pca_1402$popa)] <- "SAS"
+spp_1402[grep("STU", pca_1402$popa)] <- "SAS"
+spp_1402[grep("ITU", pca_1402$popa)] <- "SAS"
+spp_1402[grep("Jomon", pca_1402$popa)] <- "F23"
+spp_1402[grep("Afanasievo_s2", pca_1402$popa)] <- "AS2"
+spp_1402[grep("Afanasievo_s1", pca_1402$popa)] <- "AS1"
+spp_1402[grep("Afanasievo_m", pca_1402$popa)] <- "AM"
+spp_1402[grep("Afanasievo_f", pca_1402$popa)] <- "AF"
 
 # remake data.frame
 pca_1402 <- as_tibble(data.frame(pca_1402, spp_1402))
@@ -184,37 +184,37 @@ which(pca_9k$popa == "Afanasievo_f")
 spp_9k <- rep(NA, length(pca_9k$popa))
 
 #pca[,1]<- phase3[,3]
-spp_9k[grep("CHB", pca_9k$popa)] <- "East_Asia"
-spp_9k[grep("JPT", pca_9k$popa)] <- "East_Asia"
-spp_9k[grep("CHS", pca_9k$popa)] <- "East_Asia"
-spp_9k[grep("CDX", pca_9k$popa)] <- "East_Asia"
-spp_9k[grep("KHV", pca_9k$popa)] <- "East_Asia"
-spp_9k[grep("CEU", pca_9k$popa)] <- "Europe"
-spp_9k[grep("TSI", pca_9k$popa)] <- "Europe"
-spp_9k[grep("FIN", pca_9k$popa)] <- "Europe"
-spp_9k[grep("GBR", pca_9k$popa)] <- "Europe"
-spp_9k[grep("IBS", pca_9k$popa)] <- "Europe"
-spp_9k[grep("YRI", pca_9k$popa)] <- "Africa"
-spp_9k[grep("LWK", pca_9k$popa)] <- "Africa"
-spp_9k[grep("GWD", pca_9k$popa)] <- "Africa"
-spp_9k[grep("MSL", pca_9k$popa)] <- "Africa"
-spp_9k[grep("ESN", pca_9k$popa)] <- "Africa"
-spp_9k[grep("ASW", pca_9k$popa)] <- "Africa"
-spp_9k[grep("ACB", pca_9k$popa)] <- "Africa"
-spp_9k[grep("MXL", pca_9k$popa)] <- "Ad_Mixed_American"
-spp_9k[grep("PUR", pca_9k$popa)] <- "Ad_Mixed_American"
-spp_9k[grep("CLM", pca_9k$popa)] <- "Ad_Mixed_American"
-spp_9k[grep("PEL", pca_9k$popa)] <- "Ad_Mixed_American"
-spp_9k[grep("GIH", pca_9k$popa)] <- "South_Asia"
-spp_9k[grep("PJL", pca_9k$popa)] <- "South_Asia"
-spp_9k[grep("BEB", pca_9k$popa)] <- "South_Asia"
-spp_9k[grep("STU", pca_9k$popa)] <- "South_Asia"
-spp_9k[grep("ITU", pca_9k$popa)] <- "South_Asia"
-spp_9k[grep("Jomon", pca_9k$popa)] <- "Jomon"
-spp_9k[grep("Afanasievo_s2", pca_9k$popa)] <- "Afanasievo"
-spp_9k[grep("Afanasievo_s1", pca_9k$popa)] <- "Afanasievo"
-spp_9k[grep("Afanasievo_m", pca_9k$popa)] <- "Afanasievo"
-spp_9k[grep("Afanasievo_f", pca_9k$popa)] <- "Afanasievo"
+spp_9k[grep("CHB", pca_9k$popa)] <- "EAS"
+spp_9k[grep("JPT", pca_9k$popa)] <- "EAS"
+spp_9k[grep("CHS", pca_9k$popa)] <- "EAS"
+spp_9k[grep("CDX", pca_9k$popa)] <- "EAS"
+spp_9k[grep("KHV", pca_9k$popa)] <- "EAS"
+spp_9k[grep("CEU", pca_9k$popa)] <- "EUR"
+spp_9k[grep("TSI", pca_9k$popa)] <- "EUR"
+spp_9k[grep("FIN", pca_9k$popa)] <- "EUR"
+spp_9k[grep("GBR", pca_9k$popa)] <- "EUR"
+spp_9k[grep("IBS", pca_9k$popa)] <- "EUR"
+spp_9k[grep("YRI", pca_9k$popa)] <- "AFR"
+spp_9k[grep("LWK", pca_9k$popa)] <- "AFR"
+spp_9k[grep("GWD", pca_9k$popa)] <- "AFR"
+spp_9k[grep("MSL", pca_9k$popa)] <- "AFR"
+spp_9k[grep("ESN", pca_9k$popa)] <- "AFR"
+spp_9k[grep("ASW", pca_9k$popa)] <- "AFR"
+spp_9k[grep("ACB", pca_9k$popa)] <- "AFR"
+spp_9k[grep("MXL", pca_9k$popa)] <- "AMR"
+spp_9k[grep("PUR", pca_9k$popa)] <- "AMR"
+spp_9k[grep("CLM", pca_9k$popa)] <- "AMR"
+spp_9k[grep("PEL", pca_9k$popa)] <- "AMR"
+spp_9k[grep("GIH", pca_9k$popa)] <- "SAS"
+spp_9k[grep("PJL", pca_9k$popa)] <- "SAS"
+spp_9k[grep("BEB", pca_9k$popa)] <- "SAS"
+spp_9k[grep("STU", pca_9k$popa)] <- "SAS"
+spp_9k[grep("ITU", pca_9k$popa)] <- "SAS"
+spp_9k[grep("Jomon", pca_9k$popa)] <- "F23"
+spp_9k[grep("Afanasievo_s2", pca_9k$popa)] <- "AS2"
+spp_9k[grep("Afanasievo_s1", pca_9k$popa)] <- "AS1"
+spp_9k[grep("Afanasievo_m", pca_9k$popa)] <- "AM"
+spp_9k[grep("Afanasievo_f", pca_9k$popa)] <- "AF"
 
 # remake data.frame
 pca_9k <- as_tibble(data.frame(pca_9k, spp_9k))
@@ -234,51 +234,57 @@ cumsum(pve_9k$pve)
 
 
 #plotting
-ka_1402<- c(15+ nlevels(pca_1402$spp_1402))
+#ka_1402<- c(15+ nlevels(pca_1402$spp_1402))
 
 # plot pca
-b_1402 <- ggplot(pca_1402, aes(PC1, PC2, col = spp_1402, shape = spp_1402 )) +
-  geom_point(size = 3,alpha = 7/10) +
-  geom_point(data=pca_1402[1661, ], size=9,alpha = 10/10) +
-  geom_point(data=pca_1402[1658, ], size=6,alpha = 9/10) +
-  geom_point(data=pca_1402[1657, ], size=6,alpha = 9/10) +
-  geom_point(data=pca_1402[1660, ], size=6,alpha = 9/10) +
-  geom_point(data=pca_1402[1659, ], size=6,alpha = 9/10) +
+b_1402 <- ggplot(pca_1402, aes(PC1, PC2, col = spp_1402 )) +
+  geom_point(size = 0.5) +
+  geom_point(data=pca_1402[1661, ], size=3,) +
+  geom_point(data=pca_1402[1658, ], size=3,) +
+  geom_point(data=pca_1402[1657, ], size=3,) +
+  geom_point(data=pca_1402[1660, ], size=3,) +
+  geom_point(data=pca_1402[1659, ], size=3,) +
   scale_color_npg()  
 
-b_1402 <- b_1402 + coord_equal()+ theme_cowplot(font_size = 7, line_size = 1)+
-  guides(colour = guide_legend(override.aes = list(size = 1))) + 
-  scale_shape_manual(values=15:ka_1402)
+b_1402 <- b_1402 + coord_equal()+ theme_cowplot(font_size = 7, line_size = 1) +
+  theme(
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+  
 
 
-b_1402 + xlab(paste0("PC1 (", signif(pve_1402$pve[1], 3), "%)")) + ylab(paste0("PC2 (", signif(pve_1402$pve[2], 3), "%)"))
-aca_1402<-b_1402 + xlab(paste0("PC1 (", signif(pve_1402$pve[1], 3), "%)")) + ylab(paste0("PC2 (", signif(pve_1402$pve[2], 3), "%)"))
-
+b_1402 
+aca_1402<-b_1402 
 
 #9k trial
-ka_9k<- c(15+ nlevels(pca_9k$spp_9k))
+#ka_9k<- c(15+ nlevels(pca_9k$spp_9k))
 
 # plot pca
-b_9k <- ggplot(pca_9k, aes(PC1, PC2, col = spp_9k, shape = spp_9k )) +
-  geom_point(size = 3,alpha = 7/10) +
-  geom_point(data=pca_9k[1661, ], size=9,alpha = 10/10) +
-  geom_point(data=pca_9k[1658, ], size=6,alpha = 9/10) +
-  geom_point(data=pca_9k[1657, ], size=6,alpha = 9/10) +
-  geom_point(data=pca_9k[1660, ], size=6,alpha = 9/10) +
-  geom_point(data=pca_9k[1659, ], size=6,alpha = 9/10) +
+# plot pca
+b_9k <- ggplot(pca_9k, aes(PC1, PC2, col = spp_9k )) +
+  geom_point(size = 0.5) +
+  geom_point(data=pca_9k[1661, ], size=3) +
+  geom_point(data=pca_9k[1658, ], size=3) +
+  geom_point(data=pca_9k[1657, ], size=3) +
+  geom_point(data=pca_9k[1660, ], size=3) +
+  geom_point(data=pca_9k[1659, ], size=3) +
   scale_color_npg()  
 
 b_9k <- b_9k + coord_equal()+ theme_cowplot(font_size = 7, line_size = 1)+
-  guides(colour = guide_legend(override.aes = list(size = 1))) + 
-  scale_shape_manual(values=15:ka_9k)
+  theme(
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
 
 
-b_9k + xlab(paste0("PC1 (", signif(pve_9k$pve[1], 3), "%)")) + ylab(paste0("PC2 (", signif(pve_9k$pve[2], 3), "%)"))
-aca_9k<-b_9k + xlab(paste0("PC1 (", signif(pve_9k$pve[1], 3), "%)")) + ylab(paste0("PC2 (", signif(pve_9k$pve[2], 3), "%)"))
-
+b_9k 
+aca_9k<-b_9k
 
 Figure2 <- plot_grid(aca_1402, aca_9k, labels = "AUTO", ncol = 1, align = 'v')
 
 Figure2
 
-ggsave("~/Desktop/Figure2.pdf", Figure2, width=8.5, height=13, units="cm", dpi=300)
+#ggsave("~/Desktop/Figure2.pdf", Figure2, width=8.5, height=13, units="cm", dpi=300)
